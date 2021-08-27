973b3519b3154bd47cf65868984f0c15522cc3ad
luis.marcelo <luis.marcelo@shx.com.br>
2021-08-27 13:50:29 -0300

    Adicionando styled-components e criando estilos globais

---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text } from "react-native";
+
+import { Title, Container } from "./styles";
+
+export function Dashboard() {
+  return (
+    <Container>
+      <Title>Dashboard</Title>
+    </Container>
+  );
+}
