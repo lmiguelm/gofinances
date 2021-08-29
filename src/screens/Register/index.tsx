c88a654aa7c5bef03c8db0958c01f463c157eb89
lmiguelm <luismiguelfernandes.marcelo@gmail.com>
2021-08-29 18:30:15 -0300

    Controlando renderização de formulários com React Hook Forms

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
 import { Modal } from 'react-native';
+import { useForm } from 'react-hook-form';
+
 import { Button } from '../../components/Form/Button';
 import { CategorySelectButton } from '../../components/Form/CategorySelectButton';
-
 import { Header } from '../../components/Header';
 import { Input } from '../../components/Form/Input';
 import { TransactionTypeButton } from '../../components/Form/TransactionTypeButton';
@@ -10,8 +11,16 @@ import { TransactionTypeButton } from '../../components/Form/TransactionTypeButt
 import { CategorySelect } from '../CategorySelect';
 
 import { Container, Fields, Form, TransactionsTypes } from './styles';
+import { InputForm } from '../../components/Form/InputForm/indes';
+
+interface FormData {
+  name: string;
+  amount: string;
+}
 
 export function Register() {
+  const { control, handleSubmit } = useForm();
+
   const [transactionType, setTransactionType] = useState<'up' | 'down'>();
   const [categoryModalOpen, setCategoryModalOpen] = useState<boolean>(false);
 
@@ -31,14 +40,25 @@ export function Register() {
     setCategoryModalOpen(false);
   }
 
+  function handleRegister(form: FormData) {
+    const data = {
+      name: form.name,
+      amount: form.amount,
+      transactionType,
+      category,
+    };
+
+    console.log(data);
+  }
+
   return (
     <Container>
       <Header title="Cadastro" />
 
       <Form>
         <Fields>
-          <Input placeholder="Nome" />
-          <Input placeholder="Preço" />
+          <InputForm control={control} name="name" placeholder="Nome" />
+          <InputForm control={control} name="amount" placeholder="Preço" />
 
           <TransactionsTypes>
             <TransactionTypeButton
@@ -58,7 +78,7 @@ export function Register() {
           <CategorySelectButton title={category.name} onPress={handleOpenSelectCategoryModal} />
         </Fields>
 
-        <Button title="Enviar" />
+        <Button title="Enviar" onPress={handleSubmit(handleRegister)} />
       </Form>
 
       <Modal
